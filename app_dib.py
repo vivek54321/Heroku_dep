cd1d77fc5b74f2a601a12bf93fca0a66a441d33d
Vivek Sagar <45427667+vivek54321@users.noreply.github.com>
2021-09-11 13:28:51 +0530

    Add files via upload

---
+++ app_dib.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+import joblib
+
+# initialise the app
+app = Flask(__name__)
+model = joblib.load(r"C:\Users\vivek\OneDrive\Desktop\LB_MAR_14\Deployment_learnbay\Deployment\joblib\dib_79.pkl")
+
+
+@app.route('/')
+def hello_word():
+    return render_template('diabtic.html')
+
+
+
+@app.route('/predict' , methods = ['post'])
+def predict():
+    preg = request.form.get('preg')
+    plas = request.form.get('plas')
+    pres = request.form.get('pres')
+    skin = request.form.get('skin')
+    test = request.form.get('test')
+    mass = request.form.get('mass')
+    pedi = request.form.get('pedi')
+    age = request.form.get('age')
+
+    output = model.predict([[preg,plas,pres,skin,test,mass,pedi,age]])
+    if output[0]==1:
+        ans = 'dibatic'
+    else:
+        ans = 'not dibatic'
+    return render_template('predict.html', pred = f"person is {ans}")
+
+    #run the app
+app.run(debug=True)
